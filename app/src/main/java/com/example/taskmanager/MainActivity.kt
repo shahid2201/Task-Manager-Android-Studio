9cbf8acaa17fc3f363fae19b731cdd00e6163efd
Mohammad Shahid <shahimoh@sheridancollege.ca>
2024-07-19 05:57:57 -0400

    Updates String Resources

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -9,15 +9,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,7 +43,7 @@ fun TMImg(modifier: Modifier = Modifier){
             painter = painterResource(id = R.drawable.ic_task_completed),
             contentDescription = null
         )
-        TMText(taskStatus = "All tasks completed", compliment = "Nice work!")
+        TMText(taskStatus = stringResource(R.string.task_status_text), compliment = stringResource(R.string.compliment_text))
     }
 }
 
